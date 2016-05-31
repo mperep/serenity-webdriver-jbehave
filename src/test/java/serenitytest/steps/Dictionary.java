801cd9b3e3bd3a35afacdd074f2c85ed6691d104
StepanTsymbal <tsymbalstepan@gmail.com>
2016-05-31 20:24:23 +0300

    NEW

--- src/test/java/serenitytest/steps/Dictionary.java
+++ src/test/java/serenitytest/steps/Dictionary.java
@@ -6,8 +6,13 @@ import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 
 import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.model.ExamplesTable;
 import org.yecht.Data;
 import serenitytest.steps.serenity.EndUserSteps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Dictionary {
 
@@ -44,10 +49,12 @@ public class Dictionary {
     }
 
     @Then("they should see the name '$name'")
-    @Alias("they should see the name <name>")
-    public void thenTheyShouldSeeTheNameOfTeacher(String name){
+    @Alias("they should see the name <name> and cource <cource>")
+    public void thenTheyShouldSeeTheNameOfTeacher(String name, String cource){
         endUser.shouldSeeNameOfTeacher(name);
+        endUser.shouldSeeCourceForTeacher(cource);
     }
+
 
 // new scenarion to find cource
 
@@ -61,5 +68,26 @@ public class Dictionary {
 
     @Then("they should see the cource '$cource'")
     public void thenTheyShouldSeeTheCource(String cource) {endUser.shouldSeeCourceForTeacher(cource);}
+
+
+@Then ("they should see the name and cource contains: $coachTables")
+public void findAllCoaches (ExamplesTable coachTables){
+    for (Map<String,String> row : coachTables.getRows()) {
+        String name = row.get("name");
+        String cource = row.get("cource");
+        System.out.println(name +" "+ cource);
+    }
+
+}
+    /*private List toTraders(ExamplesTable table) {
+        List traders = new ArrayList();
+        for (Map<String,String> row : table.getRows()) {
+            String name = row.get("name");
+            String rank = row.get("cource");
+            //traders.add(new Trader(name, rank));
+        }
+        return traders;*/
+
+
 }
 
--- src/test/resources/stories/Teachers_SkillsUp/FindTeachrs.story
+++ src/test/resources/stories/Teachers_SkillsUp/FindTeachrs.story
@@ -12,9 +12,15 @@ Then they should see the name <name> and cource <cource>
 Examples:
 |teacher||name||cource|
 |Александр Галковский||Александр Галковский||Diving into Java|
-|Михаил Чокан||Михаил Чокан||Координатор учебного курса|
+|Михаил Чокан||Михаил Чокан||Руководитель учебного курса|
 
 Scenario: Find cource for teacher 'Александр Галковский'
 Given the user is on the Teacher page
 When the user click on card of teacher 'Александр Галковский'
 Then they should see the cource 'Diving into Java'
+
+Scenario: Find all teachers
+Given the user is on the Our Team page
+Then they should see the name and cource contains:
+|name|cource|
+|Александр Галковский|Diving into Java|
